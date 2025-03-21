0c1f58b553efb80831485fab3214292226a2294b
Deyverson Batista <deyverson@outlook.com>
2025-03-21 21:41:49 +0100

    Added Database, EFCore, Habit Entity and configurations

---
+++ DevHabit/DevHabit.Api/Entities/Habit.cs
@@ -0,0 +1,57 @@
+﻿namespace DevHabit.Api.Entities;
+
+public sealed class Habit
+{
+    public string Id { get; set; }
+    public required string Name { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public Frequency Frequency { get; set; }
+    public Target Target { get; set; }
+    public HabitStatus Status { get; set; }
+    public bool IsArchived { get; set; }
+    public DateOnly? EndDate { get; set; }
+    public Milestone? Milestone { get; set; }
+    public DateTime CreatedAtUtc { get; set; }
+    public DateTime? UpdatedAtUtc { get; set; }
+    public DateTime? LastCompletedAtUtc { get; set; }
+}
+
+public enum HabityType
+{
+    None = 0,
+    Binary,
+    Measurable
+}
+
+public enum HabitStatus
+{
+    None = 0,
+    Ongoing,
+    Completed
+}
+
+public sealed class Frequency
+{
+    public FrequencyType Type { get; set; }
+    public int TimerPerPeriod { get; set; }
+}
+
+public enum FrequencyType
+{
+    None = 0,
+    Daily,
+    Weekly,
+    Monthly
+}
+
+public sealed class Target
+{
+    public int Value { get; set; }
+    public string Unit { get; set; }
+}
+
+public sealed class Milestone
+{
+    public int Target { get; set; }
+    public int Current { get; set; }
+}
